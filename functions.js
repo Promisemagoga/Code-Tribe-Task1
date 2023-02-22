b7e5a9c08a50cbb3986b357eb5552628f0db0ff4
Promise Tshegofatso magoga <104593732+Promisemagoga@users.noreply.github.com>
2023-02-22 08:52:06 +0200

    Updated files

--- functions.js
+++ functions.js
@@ -1,39 +1,63 @@
-const name = "Promise Tshegofatso Magoga";
-document.getElementById("name").innerHTML = name;
 
-const bio = "I am a young energetic individual, working my way up to be the female version of Mark Zuckernberg. I am a fullstack web developer and I have experience building full-stack web applications and Restful APIs using languages and frameworks including html/css/javaScript, Node.js/Express.js, Testing APIs using postman and mongodb."
-document.getElementById("bio").innerHTML = bio;
-
-const img = new Image(200,200);
-img.src = "src/IMG_20220704_123351_340.webp";
-document.getElementById("bioC").appendChild(img)
-
-const video = document.createElement('video');
+const firstName = "Promise Tshegofatso";
+const lastName = "Magoga";
+const bio =
+  "I am a young energetic individual, working my way up to be the female version of Mark Zuckernberg. I am a fullstack web developer and I have experience building full-stack web applications and Restful APIs using languages and frameworks including html/css/javaScript, Node.js/Express.js, Testing APIs using postman and mongodb.";
+  const img = new Image(200,200);
+  img.src = "src/IMG_20220704_123351_340.webp";
+  const video = document.createElement('video');
 video.src = "src/1562009973860.mp4"
 video.controls = true;
 video.muted = false;
 video.width = 1400;
-document.getElementById("vid").appendChild(video)
-
-
-
-/*var a = document.createElement('a')
-var nav = document.createTextNode(["Home ", "About me", "Contact me"])
-a.appendChild(nav);
-
-[
-a.title = "Home ",
-a.title = "About me",
-a.title = "Contact me"
-]
-
-[
-a.href = "/index.html",
-a.href = "./pages/about.html",
-a.href = "./pages/contact.html"
-]
-
-document.getElementById("navigationBar").appendChild(a)*/
-
 const address = "972 Block T <br> Sosganguve,<br> 0152, Pretoria, <br> Gauteng";
+const p1 = "Made with";
+const imgOne = new Image(15,15);
+imgOne.src = "src/heart.png";
+  const p2 = "by Promise Tshegofatso Magoga";
+
+
+  
+// Render personal information onto HTML
+document.getElementById("name").innerHTML = firstName + " " + lastName;
+document.getElementById("bio").innerHTML = bio;
+document.getElementById("bioC").appendChild(img);
+document.getElementById("vid").appendChild(video);
 document.getElementById("adress").innerHTML = address;
+document.getElementById("owner").innerHTML = p1 + imgOne + p2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pages/functions.js
+++ pages/functions.js
@@ -1,3 +1,46 @@
 
 const aboutParagraph = "<p>I am a young energeting individual working my way up to be the female version of Mark Zuckernburg.<br>My journey as a geek started at mtn business app academy where I was studying mobile app development.<br>Right at that moment I knew that programming was my calling.From there I was accepted at Afica Agility,<br>for the girls in tech cohort 5 bootcamp.I took it as I am an international student representing my country.<br>And i did represent my country well as I brought back the tittle <em>backend web developer</em> and I also brought back the <br>status as my team and I came second at the Africa Agility grand finale hackathon.I am also a<br> self-taught frontend web developer.My hobby it is also educational,I am a partime fundza reader<br>,I am also a fundza cover letter 2022 competition winner. For this story you can type in my full name on google.</p>";
+const address = "972 Block T <br> Sosganguve,<br> 0152, Pretoria, <br> Gauteng";
+
+// declaring contact form variables
+const img = new Image(50,50);
+img.src = "src/9624482.png";
+const typo1 = "Email"
+const email = "tshegofatsopromise362@gmail.com"
+
+
+// Render personal information onto HTML
 document.getElementById("about").innerHTML = aboutParagraph;
+document.getElementById("adress").innerHTML = address;
+document.getElementById("education").innerHTML = education;
+document.getElementById("typo1").innerHTML = typo1;
+document.getElementById("email").innerHTML = email;
+
+const projects = [
+  { title: "Project 1", description: "Lorem ipsum dolor sit amet" },
+  { title: "Project 2", description: "Consectetur adipiscing elit" },
+  {
+    title: "Project 3",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+];
+const education = "Bachelor's Degree in Computer Science, University of XYZ";
+
+// Render personal information onto HTML
+
+
+document.getElementById("email").innerHTML = email;
+
+// Render projects onto HTML
+const projectList = document.getElementById("projectList");
+projects.forEach((project) => {
+  const projectItem = document.createElement("li");
+  const projectTitle = document.createElement("h3");
+  projectTitle.innerHTML = project.title;
+  const projectDescription = document.createElement("p");
+  projectDescription.innerHTML = project.description;
+  projectItem.appendChild(projectTitle);
+  projectItem.appendChild(projectDescription);
+  projectList.appendChild(projectItem);
+});
